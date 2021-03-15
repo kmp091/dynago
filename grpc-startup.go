c45fa462f88a349dd810fd234ca167281a872b7d
Kevin Panuelos <kevin.panuelos@gmail.com>
2021-03-15 21:47:24 +0800

    Grpc server startup

--- grpc-startup.go
+++ grpc-startup.go
@@ -1,5 +1,30 @@
-package dynago
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+
+	"github.com/kmp091/dynago/messages"
+	"github.com/kmp091/dynago/service"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 4323, "The server address in the format of host:port")
+)
 
 func main() {
+	flag.Parse()
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		fmt.Printf("Unable to listen to port %d\n", *port)
+		return
+	}
 
+	server := service.DynagoService{}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	messages.RegisterDynagoServiceServer(grpcServer, &server)
+	grpcServer.Serve(listener)
 }
--- service/dynago-service.go
+++ service/dynago-service.go
@@ -3,17 +3,19 @@ package service
 import (
 	"context"
 
-	anypb "github.com/golang/protobuf/types/known/anypb"
 	pb "github.com/kmp091/dynago/messages"
+	anypb "google.golang.org/protobuf/types/known/anypb"
 )
 
-type server struct {
+type DynagoService struct {
 	pb.UnimplementedDynagoServiceServer
 }
 
-func (s *server) Process(ctx context.Context, request *pb.DynagoRequest) (*pb.DynagoResponse, error) {
+func (s *DynagoService) Process(ctx context.Context, request *pb.DynagoRequest) (*pb.DynagoResponse, error) {
 	//TODO: run plugins
 	var response pb.DynagoResponse
-	response.Results["FinalResult"] = anypb.New(pb.ValueTypeParameter_DoubleValue{5.5})
-	return &response, nil
+	var err error
+	doubleParameter := pb.ValueTypeParameter_DoubleValue{DoubleValue: 5.5}
+	response.Results["FinalResult"], err = anypb.New(&pb.ValueTypeParameter{ParameterOneof: &doubleParameter})
+	return &response, err
 }
